bd795eb378916c30fbb11df32347648348ffd0fa
김대원 <115925564+KingDaeWon@users.noreply.github.com>
2023-09-25 19:12:13 +0900

    ㅁㄴㅇㅁㄴㅇ

---
+++ study/src/main/java/com/rest/study/board/travelboard/dto/TravelDto.java
@@ -0,0 +1,4 @@
+package com.rest.study.board.travelboard.dto;
+
+public class TravelDto {
+}
--- study/src/main/java/com/rest/study/board/travelboard/entity/Travel.java
+++ study/src/main/java/com/rest/study/board/travelboard/entity/Travel.java
@@ -1,4 +1,39 @@
 package com.rest.study.board.travelboard.entity;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@SequenceGenerator(name = "SEQ_MENU_ID", sequenceName ="SEQ_MENU_ID" , initialValue = 1, allocationSize = 1)
+@DynamicUpdate // update시 변경된 필드만 쿼리문에 포함
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Travel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_MENU_ID")
+    private Long travelId;
+    @Column(nullable = false) // Not Null
+    private String travelMemberId;
+    @Column(nullable = false) // Not Null
+    private String travelTitle;
+    @Column(nullable = false) // Not Null
+    private String travelContent;
+    @CreationTimestamp
+    private LocalDateTime travelCreatedAt;
+
 }
