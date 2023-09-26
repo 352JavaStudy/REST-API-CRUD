07e2d29bd278af2a0adb29cc9d006f25ce634888
김대원 <115925564+KingDaeWon@users.noreply.github.com>
2023-09-27 02:31:33 +0900

    Update TravelBoardController.java

--- study/src/main/java/com/rest/study/board/travelboard/controller/TravelBoardController.java
+++ study/src/main/java/com/rest/study/board/travelboard/controller/TravelBoardController.java
@@ -1,7 +1,10 @@
 package com.rest.study.board.travelboard.controller;
 
+import com.rest.study.board.travelboard.service.TravelBoardService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/travel")
 @Slf4j
 public class TravelBoardController {
+
+    @Autowired
+    private TravelBoardService travelBoardService;
+
+    
+
+
 }
