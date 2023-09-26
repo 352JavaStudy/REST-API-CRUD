8a3be49ea222449b397ca02e413f78a724ff0ed6
김대원 <115925564+KingDaeWon@users.noreply.github.com>
2023-09-27 05:41:43 +0900

    patch랑 delete손봐야함

--- study/src/main/java/com/rest/study/board/travelboard/dto/TravelBoardDto.java
+++ study/src/main/java/com/rest/study/board/travelboard/dto/TravelBoardDto.java
@@ -2,30 +2,23 @@ package com.rest.study.board.travelboard.dto;
 
 import com.rest.study.board.travelboard.entity.TravelBoard;
 import lombok.Data;
-import org.springframework.data.annotation.CreatedDate;
-
-import java.time.LocalDateTime;
 
 import javax.validation.constraints.NotBlank;
 
 
 @Data
-public class TravelBoardlDto {
+public class TravelBoardDto {
 
-    @NotBlank(message = "아이디는 필수입니다.")
     private String travelMemberId;
     @NotBlank(message = "제목은 필수입니다.")
     private String travelTitle;
     @NotBlank(message = "내용은 필수입니다.")
     private String travelContent;
-    @CreatedDate
-    private LocalDateTime travelCreatedAt;
 
     public TravelBoard toTravel(TravelBoard travelBoard) {
         travelBoard.setTravelMemberId(travelMemberId);
         travelBoard.setTravelTitle(travelTitle);
         travelBoard.setTravelContent(travelContent);
-        travelBoard.setTravelCreatedAt(travelCreatedAt);
         return travelBoard;
     }
 
@@ -34,7 +27,6 @@ public class TravelBoardlDto {
                 .travelMemberId(travelMemberId)
                 .travelTitle(travelTitle)
                 .travelContent(travelContent)
-                .travelCreatedAt(travelCreatedAt)
                 .build();
     }
 }
