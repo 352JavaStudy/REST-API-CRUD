45cec5dd8973a8a3e090ed18e63ccf6247790e49
DAMHEE-K <126038575+DAMHEE-K@users.noreply.github.com>
2023-09-28 14:33:21 +0900

    게시글 등록 완료

--- study/src/main/java/com/rest/study/board/foodboard/controller/StaticController.java
+++ study/src/main/java/com/rest/study/board/foodboard/controller/StaticController.java
@@ -1,13 +1,20 @@
 package com.rest.study.board.foodboard.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class StaticController {
 
     @RequestMapping("/foodboards")
-    public String fooBoards() { return "forward:/foodBoard_list.html"; }
+    public String getFoodBoards() { return "forward:/foodboard/foodBoard_list.html"; }
+
+    @RequestMapping(value="/foodboards/{id}", method= RequestMethod.GET)
+    public String getFoodBoard() {return "forward:/foodboard/foodBoard_detail.html";}
+
+    @RequestMapping(value="/foodboards/writeBoard", method= RequestMethod.GET)
+    public String writeFoodBoard() {return "forward:/foodboard/foodBoard_write.html";}
+
 
     @RequestMapping("/freeboards")
     public String freeBoards() {
