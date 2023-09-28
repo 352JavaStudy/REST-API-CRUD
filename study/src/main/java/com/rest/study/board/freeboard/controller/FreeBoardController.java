454e41be2e1990bb27c1717496d6c6a4ac0c4a31
JeonYera <vldzmf0877@naver.com>
2023-09-28 12:29:07 +0900

    뷰에 뿌리기

--- study/src/main/java/com/rest/study/board/freeboard/controller/FreeBoardController.java
+++ study/src/main/java/com/rest/study/board/freeboard/controller/FreeBoardController.java
@@ -5,14 +5,14 @@ import com.rest.study.board.freeboard.entity.FreeBoard;
 import com.rest.study.board.freeboard.service.FreeBoardService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:8080")
-@RequestMapping("/freeboards")
+@CrossOrigin(origins = "http://localhost")
+@RequestMapping("/api/freeboards")
 @RestController
 @Slf4j
 public class FreeBoardController {
@@ -21,8 +21,9 @@ public class FreeBoardController {
     private FreeBoardService freeBoardService;
 
     @GetMapping
-    public ResponseEntity<?> getBoards() {
-        return ResponseEntity.ok(freeBoardService.findAll());
+    public ResponseEntity<List<FreeBoard>> getBoards() {
+        List<FreeBoard> freeBoard = freeBoardService.findAll();
+        return ResponseEntity.ok(freeBoard);
     }
 
     @GetMapping("/{id}")
