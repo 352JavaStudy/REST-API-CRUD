2c75de764eafe8adc487e69c4334e8de64e14dd8
JeonYera <vldzmf0877@naver.com>
2023-09-28 17:43:17 +0900

    css 입히기

--- study/src/main/java/com/rest/study/board/foodboard/controller/StaticController.java
+++ study/src/main/java/com/rest/study/board/foodboard/controller/StaticController.java
@@ -1,12 +1,18 @@
 package com.rest.study.board.foodboard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class StaticController {
+
+    @GetMapping("/")
+    public String index() {
+        return "forward:/index.html";
+    }
 
     @RequestMapping("/foodboards")
     public String foodBoards() { return "forward:/foodBoard_list.html"; }
@@ -22,5 +28,9 @@ public class StaticController {
 
     @RequestMapping(value = "/freeboards/writeBoard", method = RequestMethod.GET)
     public String writeBoard() { return "forward:/freeboard/freeBoard_write.html";
+    }
+
+    @RequestMapping(value = "/freeboards/updateBoard/{id}", method = RequestMethod.GET)
+    public String updateBoard() { return "forward:/freeboard/freeBoard_update.html";
     }
 }
